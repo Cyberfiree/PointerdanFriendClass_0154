78724d4719f26bbad69f44b56615702f1f18d464
Cyberfire <buayagoreng99@gmail.com>
2024-05-07 12:24:17 +0800

    deklarasi kelas orang dengan akses modifier public dan private

--- pointer3/pointer3.cpp
+++ pointer3/pointer3.cpp
@@ -5,5 +5,15 @@ using namespace std;
 // Deklarasi kelas siswa agar dapat digunakan dalam deklarasi kelas orang
 class siswa;
 
+// Kelas orang dengan data member string nama dan fungsi member setNama
+class orang {
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa; // Mendeklarasikan kelas siswa sebagai friend class
+};
+
 int main() {
 }
