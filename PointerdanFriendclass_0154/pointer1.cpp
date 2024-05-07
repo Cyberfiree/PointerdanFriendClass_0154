c92cfbe8fedf8b9eeb8ccd600e8f7b25f4123874
Cyberfire <buayagoreng99@gmail.com>
2024-05-07 12:02:25 +0800

    membuat variabel nim dan menampilkannya

--- PointerdanFriendclass_0154/pointer1.cpp
+++ PointerdanFriendclass_0154/pointer1.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 class mahasiswa {
 public:
+	int nim; // Variabel anggota untuk Nomor Induk Mahasiswa (NIM)
+	void showNim() {
+		cout << "No Induk = " << nim << endl; // Fungsi untuk menampilkan NIM mahasiswa
+	}
 };
 
 int main()
