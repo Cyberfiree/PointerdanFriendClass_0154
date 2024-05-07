4387ff4d11949bfcc72cb21dd926a30a5c5c76b0
Cyberfire <buayagoreng99@gmail.com>
2024-05-07 12:43:16 +0800

    fix the command

--- pointer5/pointer5.cpp
+++ pointer5/pointer5.cpp
@@ -2,32 +2,32 @@
 #include <string>
 using namespace std;
 
-class pelajar;
-class manusia
+class pelajar; // Deklarasi kelas pelajar agar dapat digunakan di dalam kelas manusia
+class manusia // Deklarasi kelas manusia
 {
 public:
-	void showNilaiPelajar(pelajar& x);
+	void showNilaiPelajar(pelajar& x); // Deklarasi fungsi showNilaiPelajar yang akan menampilkan nilai pelajar
 };
 
-class pelajar
+class pelajar // Deklarasi kelas pelajar
 {
 private:
-	int nilai;
+	int nilai; // Anggota privat untuk menyimpan nilai pelajar
 
 public:
-	pelajar() { nilai = 100; }
-	friend void manusia::showNilaiPelajar(pelajar& x);
+	pelajar() { nilai = 100; } // Konstruktor untuk menginisialisasi nilai dengan 100
+	friend void manusia::showNilaiPelajar(pelajar& x); // Mendeklarasikan fungsi showNilaiPelajar sebagai friend function
 };
 
-void manusia::showNilaiPelajar(pelajar& x)
+void manusia::showNilaiPelajar(pelajar& x) // Implementasi fungsi showNilaiPelajar
 {
-	cout << x.nilai;
+	cout << x.nilai; // Menampilkan nilai dari objek pelajar yang diberikan
 }
 
 int main()
 {
-	manusia budi;
-	pelajar pbudi;
-	budi.showNilaiPelajar(pbudi);
-	return 0;
+	manusia budi; // Membuat objek manusia dengan nama budi
+	pelajar pbudi; // Membuat objek pelajar dengan nama pbudi
+	budi.showNilaiPelajar(pbudi); // Memanggil fungsi showNilaiPelajar untuk menampilkan nilai pelajar yang dimiliki objek pbudi
+	return 0; // Mengembalikan nilai 0 untuk menandakan bahwa program berakhir tanpa masalah
 }
